2def6ef386e6fb782b004fbe0b80625b935bbeda
jdbuenol <jdbuenol@eafit.edu.co>
2023-04-07 11:15:24 -0400

    Puerto parametrizado y puesto en 80

--- MOM/config.py
+++ MOM/config.py
@@ -1,2 +1,3 @@
 import os
 BASE_DIR=os.getcwd()
+PORT=80
--- MOM/main.py
+++ MOM/main.py
@@ -1,5 +1,6 @@
 from controllers import QueueController, TopicController, UserController
 from bottle import run
+from config import PORT
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080)
+    run(host='localhost', port=PORT)
